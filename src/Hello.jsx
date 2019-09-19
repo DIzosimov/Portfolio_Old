8c69703c48259a13ae1a08ec68fbff0319f2cbd3
David_Izosimov <David.Izosimov@gmail.com>
2019-09-19 09:24:32 +0200

    added UnDraw illustration to home page

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,11 +1,13 @@
 import React from "react"
+import { UndrawHello } from 'react-undraw-illustrations'
 
 const Hello = () => {
     return (
-        <div className="ui main container">
+        <div className="ui main container" id="greeting">
             <h1>
                 Greetings Visitor
             </h1>
+            <UndrawHello primaryColor='#12283a' height='300px' />
         </div>
     )
 }
